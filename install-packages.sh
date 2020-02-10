f8e7af98932da4b4467534bc8a2c40471d8e77a8
Rubin Bhandari <roobin.bhandari@gmail.com>
2020-02-10 19:05:04 +0545

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-yay -Sy xfce4-shooter alsa-utils pactl rofi lxappearance xbacklight kitty i3lock compton dunst caffeine redshift
+yay -Sy xfce4-shooter alsa-utils pactl rofi betterlockscreen lxappearance xbacklight kitty compton dunst caffeine redshift
 
 pip install --user bumblebee-status
 

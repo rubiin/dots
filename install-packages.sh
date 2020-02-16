3139f51ebb2e3dd5b1daa6e7c88c23cbcb4e1681
Rubin <roobin.bhandari@gmail.com>
2020-02-16 15:25:16 +0545

    added fonts directory

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-yay -Sy xfce4-shooter alsa-utils pactl rofi betterlockscreen lxappearance xbacklight kitty compton dunst caffeine redshift
+yay -Sy xfce4-shooter alsa-utils pactl rofi betterlockscreen lxappearance xbacklight alacritty compton dunst caffeine redshift
 
 pip install --user bumblebee-status
 

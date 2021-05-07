8156446120be46769eeb17030e4967bc0e1e8ad9
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-07 17:27:20 +0545

    fix: added kde backup with plasma config saver

--- scripts/executable_brew-install.sh
+++ scripts/executable_brew-install.sh
@@ -4,5 +4,5 @@
 # Installs brew necessary packages
 
 brew update && brew upgrade
-brew install kubectl tldr bat autojump minikube asdf lazydocker tldr speedtest-cli chezmoi
+brew install kubectl fd tldr bat autojump minikube asdf lazydocker tldr speedtest-cli chezmoi
 

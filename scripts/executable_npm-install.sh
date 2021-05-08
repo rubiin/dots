60fc715c09a5a654cd42f2759e4f357694b7edf2
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-08 13:04:17 +0545

    fix: chores

--- scripts/executable_npm-install.sh
+++ scripts/executable_npm-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # install all packages
-npm i -g @vue/cli @nestjs/cli ngrok yarn typescript lite-server ts-node eslint prettier npm-check-updates release-it pnpm types-installer neovim
+npm i -g @vue/cli @nestjs/cli ngrok yarn typescript lite-server ts-node eslint prettier npm-check-updates release-it pnpm types-installer neovim autocannon
 

00da30ddeab03030cbc17b442a9badab70b9418c
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-09 17:33:20 +0545

    feat: chores

--- scripts/executable_arch-install-essentials.sh
+++ scripts/executable_arch-install-essentials.sh
@@ -2,4 +2,6 @@
 
 # Installs necessary packages like font and ides
 
-yay -Syyu brew-git alacritty nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition
+yay -S brave-bin
+yay -S plank-git
+yay -Syyu alacritty nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition
--- scripts/executable_brew-install.sh
+++ scripts/executable_brew-install.sh
@@ -2,6 +2,7 @@
 
 # Installs brew necessary packages
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update && brew upgrade
 brew install kubectl fd tldr bat autojump minikube asdf lazydocker tldr speedtest-cli chezmoi
 

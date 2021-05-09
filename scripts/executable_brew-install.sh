fb32764eb18100be704a0405ad57c608a1c8c7b7
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-09 21:28:53 +0545

    feat: chores

--- scripts/executable_brew-install.sh
+++ scripts/executable_brew-install.sh
@@ -3,6 +3,7 @@
 # Installs brew necessary packages
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> $HOME/.zshrc
 brew update && brew upgrade
 brew install kubectl fd tldr bat autojump minikube asdf lazydocker tldr speedtest-cli chezmoi
 

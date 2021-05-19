7f741d3ce674f63c0b64ca133517768d40583c08
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-19 17:45:20 +0545

    feat: added gitflow

--- scripts/base-install.sh
+++ scripts/base-install.sh
@@ -2,9 +2,9 @@
 
 # Installs necessary packages like font and ides
 
-yay -S brave-bin chezmoi exa waterfox-classic-bin
+yay -Syuu brave-bin chezmoi exa waterfox-classic-bin
 yay -S plank-git
-yay -Syyu alacritty xclip asdf-vm docker docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-twemoji-color ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition kite --ignore linux
+yay -S gitflow-avh alacritty xclip asdf-vm docker docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-twemoji-color ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition kite --ignore linux
 
 echo 'Yay Installation Done with all its packages'
 

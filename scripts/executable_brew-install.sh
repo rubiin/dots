661f07f95681453e74a49596fe16db618d065dc4
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-19 11:31:35 +0545

    feat: added exa

--- scripts/executable_brew-install.sh
+++ scripts/executable_brew-install.sh
@@ -5,7 +5,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> $HOME/.zshrc
 brew update && brew upgrade
-brew install kubectl fd tldr bat autojump minikube lazydocker tldr speedtest-cli lsd
+brew install kubectl fd tldr bat autojump minikube lazydocker tldr speedtest-cli
 
 echo 'Brew Installation Done with all its packages'
 

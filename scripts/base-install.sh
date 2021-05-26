e1dca9d1d4e5a48eb9e6d634929ee51bf65e418f
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-05-26 16:48:06 +0545

    fix: added default packages for nodejs

--- scripts/base-install.sh
+++ scripts/base-install.sh
@@ -4,7 +4,7 @@
 
 yay -S brave-bin chezmoi
 yay -S plank-git
-yay -Syyu alacritty asdf-vm docker docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors-bin spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition kite --ignore linux
+yay -Syyu alacritty asdf-vm docker docker-compose python-pip mousepad nano skypeforlinux-stable-bin zoom mojave-gtk-theme-git visual-studio-code-bin adwaita-icon-theme capitaine-cursors gparted htop la-capitaine-icon-theme neovim postman-bin rate-arch-mirrors spectacle ttf-bitstream-vera ttf-dejavu ttf-font-awesome ttf-hack ttf-liberation ttf-ms-fonts ttf-nerd-fonts-symbols-mono ttf-twemoji-color uuid vlc wps-office wps-office-fonts youtube-dl gwenview ktorrent persepolis intellij-idea-ultimate-edition kite --ignore linux
 
 echo 'Yay Installation Done with all its packages'
 
--- scripts/executable_npm-install.sh
+++ scripts/executable_npm-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # install all packages
-npm i -g @vue/cli @nestjs/cli ngrok yarn json-server typescript nodemon lite-server ts-node eslint prettier npm-check-updates release-it pnpm types-installer neovim autocannon
+npm i -g @vue/cli @nestjs/cli ngrok yarn typescript nodemon lite-server ts-node eslint prettier npm-check-updates release-it pnpm types-installer neovim autocannon
 
 echo 'Npm Installation Done with all its packages'
